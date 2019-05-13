045436d81ddd4a121040f7e48ad139706e4f5d3c
bartek8654 <uttsmile8654@gmail.com>
2019-05-13 11:10:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/gazetkapl/Main2Activity.java
@@ -0,0 +1,20 @@
+package com.example.gazetkapl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    PDFView pdfviewer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        pdfviewer=(PDFView)findViewById(R.id.pdfviewer);
+        pdfviewer.fromAsset("demo.pdf").load();
+    }
+}
---
+++ app/src/main/java/com/example/gazetkapl/Main3Activity.java
@@ -0,0 +1,21 @@
+package com.example.gazetkapl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+public class Main3Activity extends AppCompatActivity {
+
+    PDFView pdfviewer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        pdfviewer=(PDFView)findViewById(R.id.pdfviewer);
+        pdfviewer.fromAsset("demo2.pdf").load();
+
+    }
+}
